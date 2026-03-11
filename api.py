bfcd16934188cdaf461d5b2f0feac00d9f413d55
georges-chcr <georges.chcr@gmail.com>
2026-03-11 17:14:58 +0100

    feat: déploiement de l'API FastAPI et finalisation du projet ML

--- api.py
+++ api.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import warnings
+
+warnings.filterwarnings("ignore", message="X does not have valid feature names")
 
 app = FastAPI(
     title="Fraud Detection ML Engine",
